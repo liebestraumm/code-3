9b4474b59d91697b5712fad1e425955b086fae1c
Carlos <carlosarturo.agelvisvergara@connect.qut.edu.au>
2017-08-04 00:50:16 +1000

    Fixed table errors

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,9 +1,16 @@
 <p>hello</p>
-<table>
+<table border="1">
 <tr>
 <td>Come Out</td>
 <td>Come Out</td>
 <td>Wherever you are!</td>
+</tr>
+
+<tr>
+<td>You say yes</td>
+<td>I say no</td>
+<td>You say stop</td>
+<td>I say no </td>
 </tr>
 
 </table>
