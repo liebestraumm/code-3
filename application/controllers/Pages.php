eed630da087d8247def01b6680019d25a0799b39
Carlos <carlosarturo.agelvisvergara@connect.qut.edu.au>
2017-08-04 00:31:17 +1000

    Changes added to Pages.php

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -5,11 +5,10 @@ class Pages extends CI_Controller {
         {
 			if ( ! file_exists(APPPATH.'views/pages/'.$page.'.php'))
         {
-                // Whoops, we don't have a page for that!
                 show_404();
         }
 
-        $data['title'] = ucfirst($page); // Capitalize the first letter
+        $data['title'] = ucfirst($page);
 
         $this->load->view('templates/header', $data);
         $this->load->view('pages/'.$page, $data);
